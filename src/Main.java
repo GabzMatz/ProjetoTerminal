41fd379c840c61a45527fe20222a9144c3c7baf3
Gabriel <gabriel.antiqueira8@gmail.com>
2024-08-20 09:32:30 -0300

    tes

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ola user:");
+        System.out.println("Digite um valor");
+        int valor = sc.nextInt();
+        System.out.println("Seu valor: "+valor);
+
+
+    }
+}
